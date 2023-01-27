d23f849bd6470c68959db6b815d279f3c63c33bc
Pichuman100 <sparqstar@gmail.com>
2023-01-27 14:42:34 -0500

    pixel gf fixed

--- iso/chars/bfpixel/bfpixel.h
+++ iso/chars/bfpixel/bfpixel.h
@@ -1,14 +1,15 @@
-iso/chars/bfpixel/main.arc: iso/chars/bfpixel/idle0.tim iso/chars/bfpixel/left0.tim iso/chars/bfpixel/down0.tim iso/chars/bfpixel/up0.tim iso/chars/bfpixel/right0.tim iso/chars/bfpixel/leftm0.tim iso/chars/bfpixel/downm0.tim iso/chars/bfpixel/upm0.tim iso/chars/bfpixel/rightm0.tim 
+iso/chars/bfpixel/main.arc: iso/chars/bfpixel/idles0.tim iso/chars/bfpixel/lefts0.tim iso/chars/bfpixel/downs0.tim iso/chars/bfpixel/ups0.tim iso/chars/bfpixel/rights0.tim iso/chars/bfpixel/leftsm0.tim iso/chars/bfpixel/downsm0.tim iso/chars/bfpixel/upsm0.tim iso/chars/bfpixel/rightsm0.tim iso/chars/bfpixel/shoot0.tim 
 
-  "idle0.tim",
-  "left0.tim",
-  "down0.tim",
-  "up0.tim",
-  "right0.tim",
-  "leftm0.tim",
-  "downm0.tim",
-  "upm0.tim",
-  "rightm0.tim",
+  "idles0.tim",
+  "lefts0.tim",
+  "downs0.tim",
+  "ups0.tim",
+  "rights0.tim",
+  "leftsm0.tim",
+  "downsm0.tim",
+  "upsm0.tim",
+  "rightsm0.tim",
+  "shoot0.tim",
 
 
   BFPixel_ArcMain_Idle0,
@@ -20,34 +21,40 @@ iso/chars/bfpixel/main.arc: iso/chars/bfpixel/idle0.tim iso/chars/bfpixel/left0.
   BFPixel_ArcMain_DownM0,
   BFPixel_ArcMain_UpM0,
   BFPixel_ArcMain_RightM0,
+  BFPixel_ArcMain_Shoot0,
 
 
-  {BFPixel_ArcMain_Idle0, {  0,  0, 55, 56}, {167,154}}, //0 idle 1
-  {BFPixel_ArcMain_Idle0, { 55,  0, 53, 56}, {166,154}}, //1 idle 2
-  {BFPixel_ArcMain_Idle0, {108,  0, 52, 58}, {164,156}}, //2 idle 3
-  {BFPixel_ArcMain_Idle0, {160,  0, 52, 59}, {164,157}}, //3 idle 4
-  {BFPixel_ArcMain_Idle0, {  0, 59, 53, 59}, {165,157}}, //4 idle 5
+  {BFPixel_ArcMain_Idle0, {  0,  0, 55, 56}, {165,154}}, //0 idles 1
+  {BFPixel_ArcMain_Idle0, { 55,  0, 53, 56}, {164,154}}, //1 idles 2
+  {BFPixel_ArcMain_Idle0, {108,  0, 53, 58}, {163,156}}, //2 idles 3
+  {BFPixel_ArcMain_Idle0, {161,  0, 53, 59}, {163,157}}, //3 idles 4
+  {BFPixel_ArcMain_Idle0, {  0, 59, 54, 59}, {164,157}}, //4 idles 5
 
-  {BFPixel_ArcMain_Left0, {  0,  0, 53, 59}, {168,157}}, //5 left 1
-  {BFPixel_ArcMain_Left0, { 53,  0, 52, 59}, {167,157}}, //6 left 2
+  {BFPixel_ArcMain_Left0, {  0,  0, 53, 59}, {166,157}}, //5 lefts 1
+  {BFPixel_ArcMain_Left0, { 53,  0, 52, 59}, {165,157}}, //6 lefts 2
 
-  {BFPixel_ArcMain_Down0, {  0,  0, 52, 55}, {165,153}}, //7 down 1
-  {BFPixel_ArcMain_Down0, { 52,  0, 52, 56}, {166,154}}, //8 down 2
+  {BFPixel_ArcMain_Down0, {  0,  0, 53, 58}, {164,153}}, //7 downs 1
+  {BFPixel_ArcMain_Down0, { 53,  0, 51, 58}, {163,154}}, //8 downs 2
 
-  {BFPixel_ArcMain_Up0, {  0,  0, 54, 62}, {163,160}}, //9 up 1
-  {BFPixel_ArcMain_Up0, { 54,  0, 55, 61}, {164,159}}, //10 up 2
+  {BFPixel_ArcMain_Up0, {  0,  0, 57, 62}, {164,160}}, //9 ups 1
+  {BFPixel_ArcMain_Up0, { 57,  0, 58, 61}, {165,159}}, //10 ups 2
 
-  {BFPixel_ArcMain_Right0, {  0,  0, 52, 58}, {161,156}}, //11 right 1
-  {BFPixel_ArcMain_Right0, { 52,  0, 53, 58}, {162,156}}, //12 right 2
+  {BFPixel_ArcMain_Right0, {  0,  0, 50, 59}, {157,156}}, //11 rights 1
+  {BFPixel_ArcMain_Right0, { 50,  0, 51, 59}, {158,156}}, //12 rights 2
 
-  {BFPixel_ArcMain_LeftM0, {  0,  0, 51, 60}, {167,158}}, //13 leftm 1
-  {BFPixel_ArcMain_LeftM0, { 51,  0, 52, 59}, {167,157}}, //14 leftm 2
+  {BFPixel_ArcMain_LeftM0, {  0,  0, 51, 59}, {165,157}}, //13 leftsm 1
+  {BFPixel_ArcMain_LeftM0, { 51,  0, 52, 59}, {165,157}}, //14 leftsm 2
 
-  {BFPixel_ArcMain_DownM0, {  0,  0, 53, 58}, {167,156}}, //15 downm 1
-  {BFPixel_ArcMain_DownM0, { 53,  0, 52, 57}, {166,155}}, //16 downm 2
+  {BFPixel_ArcMain_DownM0, {  0,  0, 52, 61}, {164,156}}, //15 downsm 1
+  {BFPixel_ArcMain_DownM0, { 52,  0, 51, 59}, {163,155}}, //16 downsm 2
 
-  {BFPixel_ArcMain_UpM0, {  0,  0, 54, 62}, {163,160}}, //17 upm 1
-  {BFPixel_ArcMain_UpM0, { 54,  0, 55, 61}, {164,159}}, //18 upm 2
+  {BFPixel_ArcMain_UpM0, {  0,  0, 56, 62}, {163,160}}, //17 upsm 1
+  {BFPixel_ArcMain_UpM0, { 56,  0, 58, 61}, {165,159}}, //18 upsm 2
 
-  {BFPixel_ArcMain_RightM0, {  0,  0, 54, 60}, {162,158}}, //19 rightm 1
-  {BFPixel_ArcMain_RightM0, { 54,  0, 53, 59}, {162,157}}, //20 rightm 2
+  {BFPixel_ArcMain_RightM0, {  0,  0, 52, 61}, {158,158}}, //19 rightsm 1
+  {BFPixel_ArcMain_RightM0, { 52,  0, 51, 60}, {158,157}}, //20 rightsm 2
+
+  {BFPixel_ArcMain_Shoot0, {  0,  0, 60, 58}, {167,156}}, //21 shoot 1
+  {BFPixel_ArcMain_Shoot0, { 60,  0, 62, 58}, {169,156}}, //22 shoot 2
+  {BFPixel_ArcMain_Shoot0, {122,  0, 63, 58}, {170,156}}, //23 shoot 3
+  {BFPixel_ArcMain_Shoot0, {185,  0, 56, 59}, {166,157}}, //24 shoot 4
